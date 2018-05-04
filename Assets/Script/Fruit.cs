b431b06627307937b98b7a327cecfadb1fe0e6f2
Santo Commarato <cylynex@gmail.com>
2018-05-04 09:46:38 -0600

    Slicing of first fruit

--- Assets/Script/Fruit.cs
+++ Assets/Script/Fruit.cs
@@ -6,5 +6,24 @@ public class Fruit : MonoBehaviour {
 
     public GameObject slicedFruitPrefab;
 
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            CreateSlicedFruit();
+        }
+    }
+
+    public void CreateSlicedFruit() {
+        GameObject froot = (GameObject)Instantiate(slicedFruitPrefab,transform.position,transform.rotation);
+
+        // create rigidbodies for explosion
+        Rigidbody[] sliced = froot.transform.GetComponentsInChildren<Rigidbody>();
+
+        foreach(Rigidbody rb in sliced) {
+            rb.transform.rotation = Random.rotation;
+            rb.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+        }
+
+        Destroy(gameObject);
+    }
 
 }
