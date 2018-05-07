f3c533e134d8eb510fbc7eb84dc7d84ee35486a6
Santo Commarato <cylynex@gmail.com>
2018-05-07 17:05:22 -0600

    Bombs and sounds

--- Assets/Script/Bomb.cs
+++ Assets/Script/Bomb.cs
@@ -7,7 +7,22 @@ public class Bomb : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D collision) {
         Blade b = collision.GetComponent<Blade>();
         if (b) {
-            Debug.Log("hit by blade");
+
+            // Sound
+            AudioSource boom = GetComponent<AudioSource>();
+            boom.Play();
+
+            // Particles
+            ParticleSystem explosion = GetComponent<ParticleSystem>();
+            explosion.Play();
+
+            // Poof the bomb itself
+            MeshRenderer dabomb = GetComponent<MeshRenderer>();
+            dabomb.enabled = false;
+            Destroy(gameObject,2f);
+
+            // Charge player lives
+            Game.lives--;
         }
     }
 
