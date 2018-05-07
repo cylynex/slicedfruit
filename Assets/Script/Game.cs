6cf72952fdfe3c8ad4b4f03044449fcb120ecd25
Santo Commarato <cylynex@gmail.com>
2018-05-07 10:52:33 -0600

    Added UI, score, lives, lose screen

--- Assets/Script/Game.cs
+++ Assets/Script/Game.cs
@@ -2,30 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Game : MonoBehaviour {
 
-    public GameObject fruitToSpawn;
+    public GameObject[] fruitToSpawn;
     public Transform[] spawnPlaces;
     public static int score;
+    public static int lives;
 
     [Header("Settings")]
     public float minWait = 1f;
     public float maxWait = 2f;
     public float minForce = 5;
     public float maxForce = 20;
+    public int nextLife = 100;
 
     [Header("UI")]
     public Text scoreBoard;
+    public Text livesCounter;
+    public Text nextLifeCounter;
 
     void Start() {
         StartCoroutine(SpawnFruits());
         score = 0;
+        lives = 3;
+        nextLife = 100;
     }
 
 
     void Update() {
+
+        if (lives <= 0) {
+            Time.timeScale = 0;
+            SceneManager.LoadScene("Lose");
+        }
+
+        if (score >= nextLife) {
+            nextLife = nextLife * 2;
+            lives++;
+            nextLifeCounter.text = nextLife.ToString();
+        }
+
+        // Update High Score
+        if (score > highScore) {
+            highScore = score;
+        }
+
         scoreBoard.text = score.ToString();
+        livesCounter.text = lives.ToString();
     }
 
 
@@ -36,8 +61,8 @@ public class Game : MonoBehaviour {
             // Pick a random spawn place
             Transform t = spawnPlaces[Random.Range(0, spawnPlaces.Length)];
 
-            // Spawn
-            GameObject fruit = Instantiate(fruitToSpawn, t.position, t.rotation);
+            // Spawn a random fruit
+            GameObject fruit = Instantiate(fruitToSpawn[Random.Range(0,fruitToSpawn.Length)], t.position, t.rotation);
 
             // Toss it
             fruit.GetComponent<Rigidbody2D>().AddForce(t.transform.up * Random.Range(minForce,maxForce), ForceMode2D.Impulse);
---
+++ Assets/Script/Loselife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loselife : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D coll) {
+        Debug.Log(coll.gameObject.tag);
+        Game.lives--;
+    }
+}
